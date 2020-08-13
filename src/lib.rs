f33c6c4df3604a51a2703f7065bbd042befa8a79
Sven Lechner <SirWindfield@users.noreply.github.com>
2020-08-13 17:29:20 +0200

    chore: add implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,48 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+//! A helper trait to convert values to `usize`.
+//!
+//! This is most useful for code that works with array indices. The trait allows
+//! to pass anything into the function that can be resolved to a `usize`.
+
+/// A trait used to convert a type into a `usize`.
+pub trait ToUsize {
+    /// Converts self into `usize`.
+    fn to_usize(&self) -> usize;
+}
+
+impl ToUsize for u8 {
+    #[inline]
+    fn to_usize(&self) -> usize {
+        *self as usize
+    }
+}
+
+impl ToUsize for u16 {
+    #[inline]
+    fn to_usize(&self) -> usize {
+        *self as usize
+    }
+}
+
+impl ToUsize for u32 {
+    #[inline]
+    fn to_usize(&self) -> usize {
+        *self as usize
+    }
+}
+
+// 32bit platforms use a `usize` of 32bit. Converting using a cast would fail on
+// those systems.
+#[cfg(target_pointer_width = "64")]
+impl ToUsize for u64 {
+    #[inline]
+    fn to_usize(&self) -> usize {
+        *self as usize
+    }
+}
+
+impl ToUsize for usize {
+    #[inline]
+    fn to_usize(&self) -> usize {
+        *self
     }
 }
